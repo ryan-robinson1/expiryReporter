650e53079ca0f0b125c1f7c3eab226485898e377
Michael Wyrick <Michael.Wyrick@caci.com>
2021-08-12 12:27:52 -0400

    Formatting and other stuff

--- expiryReporter.go
+++ expiryReporter.go
@@ -16,6 +16,7 @@ type Request struct {
 	url      string
 	duration time.Duration
 }
+
 type Result struct {
 	url    string
 	valid  bool
@@ -26,15 +27,14 @@ type Result struct {
 
 func fileConverter(filepath string) ([]Request, error) {
 	var reqs []Request
-	file, err := os.Open(filepath)
 
+	file, err := os.Open(filepath)
 	if err != nil {
-		return []Request{}, nil
+		return nil, err
 	}
 	defer file.Close()
 
 	reader := bufio.NewReader(file)
-
 	for {
 		line, eoferr := reader.ReadString('\n')
 		line = strings.TrimSuffix(line, "\n")
@@ -44,23 +44,29 @@ func fileConverter(filepath string) ([]Request, error) {
 		if err != nil {
 			return []Request{}, errors.New("err: invalid file format")
 		}
+
 		req := Request{s[1], dur}
 		reqs = append(reqs, req)
+
 		if eoferr != nil {
-			break
+			return reqs, nil
 		}
 	}
-	return reqs, nil
+
 }
 
-func doCheck(r Request, resultsChan chan Result) {
+func doCheck(r Request, rc chan Result) {
 	status, expiry, err := getCertExpiry.GetCertExpiry(r.url, "", "", "", true)
-	res := Result{r.url, status == 0, time.Now().UTC().Format("2006-01-02 15:04:05"), expiry, err}
-	resultsChan <- res
+	rc <- Result{r.url,
+		status == 0,
+		time.Now().UTC().Format("2006-01-02 15:04:05"),
+		expiry,
+		err}
 }
-func sleepAndExecute(r Request, resultsChan chan Result) {
+
+func sleepAndExecute(r Request, rc chan Result) {
 	for {
-		doCheck(r, resultsChan)
+		doCheck(r, rc)
 		time.Sleep(r.duration)
 	}
 }
@@ -72,29 +78,43 @@ func BuildReport(r Result) string {
 	}
 	return "-------------------------------------\n" + r.url + " report\n-------------------------------------\ntime        : " + r.time + "\nexpired     : " + strconv.FormatBool(!r.valid) + "\nexpiration  : " + r.expiry + "\n\n"
 }
+
 func main() {
-	reqs, err := fileConverter("input")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	//	reqs, err := fileConverter("input")
+	//	if err != nil {
+	//		fmt.Println(err)
+	//		os.Exit(1)
+	//	}
+
+	reqs := []Request{{"cnn.com:443", 7 * time.Second},
+		{"expired.badssl.com:443", 13 * time.Second},
+		{"example.com:443", 29 * time.Second},
+		{"mainrouter-silkwave.novatr:61617", 22 * time.Second}}
+
 	exit := make(chan bool)
 	results := make(chan Result, 10)
 	ok := make(chan string, 10)
 	er := make(chan string, 10)
 
 	for _, r := range reqs {
-		go sleepAndExecute(r, results)
+		go func(r Request) {
+			for {
+				doCheck(r, results)
+				time.Sleep(r.duration)
+			}
+		}(r)
 	}
 
 	go func() {
 		for r := range results {
-			R := BuildReport(r)
-			if r.valid {
-				ok <- R
-			} else {
-				er <- R
-			}
+			r := r // YES there is a valid and needed reason this is here. Left as an exercise to the student to find out why
+			go func() {
+				if r.valid {
+					ok <- BuildReport(r)
+				} else {
+					er <- BuildReport(r)
+				}
+			}()
 		}
 	}()
 
@@ -103,6 +123,7 @@ func main() {
 			fmt.Fprintln(os.Stdout, R)
 		}
 	}()
+
 	go func() {
 		for R := range er {
 			fmt.Fprintln(os.Stderr, R)
